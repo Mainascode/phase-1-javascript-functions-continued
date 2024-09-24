c216f93c8c73d812f001d57c1b9753eec3879793
Maina <smurkcarter855@gmail.com>
2024-09-24 14:45:02 +0300

    origin

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`
+}
+function mondayWork(activity='go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+function wrapAdjective(wrap ='special'){
+    return function inner(message){
+       return `You are ${wrap}${message}${wrap}!`
+    }
+}
